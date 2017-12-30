248d1f988e572f7db43f6656a499f284f3226bb3
Matt Fetterman <navx2810@gmail.com>
2017-12-30 16:18:59 -0500

    Updated regex and display logic.
    Added the operation and number capture group.
    Added in critical success and fail messages.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ client.on("message", message => {
         const params = generateParams(message.content)
         console.log(params)
         switch (params[0].toLowerCase().replace(/^!/, '')) {
+            case "r":
             case "roll":
                 log("Rolling...")
                 const res = RollDie(params[1])
@@ -24,6 +25,12 @@ client.on("message", message => {
                     message.reply(`You made a bad roll with '${params[1] ? params[1] : 'nothing'}'. Please roll again.`)
                 } else {
                     message.reply(`Rolled *${res.casts}* = **${res.result}**`)
+                    if(res.successes.length) {
+                        message.reply(`You rolled **${res.successes.length}** critical *successes!*`)
+                    }
+                    if(res.fails.length) {
+                        message.reply(`You rolled **${res.fails.length}** critical *failures!*`)
+                    }
                 }
                 break
         }
@@ -37,12 +44,12 @@ function generateParams(content) {
 function RollDie(roll) {
     log(`Rolling Dice: ${roll}`)
     // Test successes: (1d8), (1d8+2), (1d8*2), (1d8-2), (1d8+2)*2
-    const rgx = /(\d*)d(\d+)([*+-]\d+)?/i
+    const rgx = /(\d*)d(\d+)([*+-])?(\d+)?/i
     if(rgx.test(roll)) {
-        const rolls = rgx.exec(roll).slice(1,4)
+        const rolls = rgx.exec(roll).slice(1,5)
         if(rolls && rolls.length) {
             log(`Roll Successful: ${rolls}`)
-            let [count, die, addition] = rolls
+            let [count, die, operation, addition] = rolls
             if(!count) { count = 1 }
             let casts = []
             let val = 0
@@ -51,14 +58,15 @@ function RollDie(roll) {
                 casts.push( roll )
                 val+=roll
             }
-            if(addition) {
-                addition = addition.split('').join(' ')
-                val = eval("".concat(val.toString(), addition))
+            if(operation) {
+                val = eval("".concat(val.toString(), operation.toString(), addition.toString()))
             }
 
+            const successes = casts.filter(it => it === 20)
+            const fails = casts.filter(it => it === 1)
             casts = casts.join(' + ')
-            if(addition) { casts = `${casts} ${addition}` }
-            return { casts, result: val }
+            if(addition) { casts = `${casts} ${operation} ${addition}` }
+            return { casts, result: val, successes, fails }
         }
         return 0
     }
